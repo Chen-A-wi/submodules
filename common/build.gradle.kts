643d05846fa4776f3d070ac10c8430597f374b18
BenChen <tttw216@gmail.com>
2023-02-08 16:56:37 +0800

    [Fix] to stable

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,40 +1,40 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+	id("com.android.library")
+	id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.awilab.common"
-    compileSdk = 33
+	namespace = "com.awilab.common"
+	compileSdk = 33
 
-    defaultConfig {
-        minSdk = 24
+	defaultConfig {
+		minSdk = 24
 
-        testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles ("consumer-rules.pro")
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.appcompat)
 
-    testImplementation(project(":submodules:testing"))
+	testImplementation(project(":testing"))
 }
