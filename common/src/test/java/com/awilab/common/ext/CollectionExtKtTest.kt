725cf23b971386db1ac5298baa54a3e578bd312b
BenChen <tttw216@gmail.com>
2023-02-20 16:03:55 +0800

    [Feat] create collection ext test

---
+++ common/src/test/java/com/awilab/common/ext/CollectionExtKtTest.kt
@@ -0,0 +1,19 @@
+package com.awilab.common.ext
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class CollectionExtKtTest {
+
+	@BeforeEach
+	fun setUp() {
+	}
+
+	@Test
+	fun withNotNullAndEmpty() {
+	}
+
+	@Test
+	fun whenNotNullAndEmpty() {
+	}
+}
