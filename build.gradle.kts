117aa86ac38126304790b5896b7355f40d1f5733
BenChen <tttw216@gmail.com>
2023-02-24 11:11:36 +0800

    [Fix] change junit5 buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
 	id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 	id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
 }
+
+buildscript {
+	dependencies {
+		classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
+	}
+}
